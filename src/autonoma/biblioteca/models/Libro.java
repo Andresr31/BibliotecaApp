65720e94c52c081a76258e99f91434e46bb52f86
Juan Hernandez <juanestebanhernandezmartinez@gmail.com>
2025-03-12 17:52:35 -0500

    Clase libro si es

--- src/autonoma/biblioteca/models/Libro.java
+++ src/autonoma/biblioteca/models/Libro.java
@@ -5,9 +5,52 @@
 package autonoma.biblioteca.models;
 
 /**
+ * 
+ * Modelo que permite representar un libro
  *
- * @author juane
+ * @author Juan Esteban Hernández Martínez
+ * @since 20250312
+ * @version 1.0.0 
  */
 public class Libro {
+    
+    /////////////////////////////////////////////////////////////////////////
+    ///// atributos
+    
+    /*
+    * id del libro
+    */
+    private long id;
+    /*
+    * Titulo del libro
+    */
+    private String titulo;
+    
+    
+    /////////////////////////////////////////////////////////////////////////
+    /////////////// metodo constructor
+    public Libro(long id, String titulo){
+        this.id = id;
+        this.titulo = titulo;
+    }
+    
+    ////////////////////////////////////////////////////////////////////////
+    ////////////// metodo de acceso
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
     
 }
