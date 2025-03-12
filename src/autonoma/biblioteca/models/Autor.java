e075c304ccbe81bac6b542ec430c31252cadbba5
Juan Sebastian Lopez Guzman <juanse150906@gmail.com>
2025-03-12 17:52:42 -0500

    Clase Autor

--- src/autonoma/biblioteca/models/Autor.java
+++ src/autonoma/biblioteca/models/Autor.java
@@ -5,9 +5,47 @@
 package autonoma.biblioteca.models;
 
 /**
+ * modelo que representa un Autor
  *
- * @author Asus
+ * @author Juan Sebastian Lopez Guzman
+ * @since 20250312
+ * @version 1.0.0
+ * 
  */
 public class Autor {
+    ///////////Atributos
+    /**
+     * Nombre de la editorial
+     */
+    private String editorial;
+    /**
+     * nombre de la profesion 
+     */
+    private String profesion;
+    
+    ///////////Metodo constructor
+    public Autor(String editorial, String profesion) {
+        this.editorial = editorial;
+        this.profesion = profesion;
+    }
+    /////////////Metodos de acceso
+
+    public String getEditorial() {
+        return editorial;
+    }
+
+    public void setEditorial(String editorial) {
+        this.editorial = editorial;
+    }
+
+    public String getProfesion() {
+        return profesion;
+    }
+
+    public void setProfesion(String profesion) {
+        this.profesion = profesion;
+    }
+    
+    
     
 }
